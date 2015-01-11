59b1d1e2dc44099f15efab8697456e112f3048de
Eric Davis <eric@davising.com>
2015-01-11 09:51:58 -0800

    Initial commit

---
+++ pandoc-opml.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import pprint
+
+head, body = json.loads(sys.stdin.read())
+
+def extract(contents):
+    ret = []
+    for obj in contents:
+        if obj.get('t') == 'Str':
+            ret.append(obj.get('c'))
+        elif obj.get('t') == 'Space':
+            ret.append(' ')
+        else:
+            ret.append('!!!')
+    return ''.join(ret)
+
+class Node(object):
+    def __init__(self, text):
+        self.text = text
+        self.children = []
+
+    def append(self, node):
+        self.children.append(node)
+
+nodes = []
+
+def build(nodes):
+    def process(node, depth = 1):
+        for child in node.children:
+            print (' ' * depth * 2) + child.text
+            process(child, depth + 1)
+
+    for summit in nodes.pop(0):
+        print summit.text
+        process(summit)
+
+def parse(content, depth = 0):
+    for obj in content:
+        if obj.get('t') == 'BulletList':
+            for element in obj.get('c'):
+                parse(element, depth + 1)
+        elif obj.get('t') == 'Plain':
+            node = Node(extract(obj.get('c')))
+
+            try:
+                nodes[depth - 1].append(node)
+            except IndexError:
+                nodes.append([node])
+
+            if (depth - 1) > 0:
+                # minus 2 to make it zero-indexed and then get the parent
+                parent = nodes[depth - 2][-1]
+                parent.append(node)
+
+parse(body)
+build(nodes)
